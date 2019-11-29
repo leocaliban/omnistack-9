4f3f398daa88bedb0ed83e24610ec259d05201c1
Leocaliban <leocaliban@gmail.com>
2019-11-28 23:13:04 -0300

    Configura rotas e adiciona mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb+srv://leocaliban:leocaliban@cluster0-lt070.mongodb.net/09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    return res.json({
-        message: 'SERVER WORKS!!!'
-    });
-});
+app.use(routes);
 
 app.listen(3333);
