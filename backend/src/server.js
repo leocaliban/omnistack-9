2ce01f93d7be11164596218092c58b75dfc760f9
Leocaliban <leocaliban@gmail.com>
2019-11-28 22:14:37 -0300

    Test API endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,4 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.json({
+        message: 'SERVER WORKS!!!'
+    });
+});
+
 app.listen(3333);
