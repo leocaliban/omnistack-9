8d0058a1e05a074221f5b549e66f13f6dcb67c43
Leonardo Batista de Araujo <lbaraujo2@stefanini.com>
2019-11-29 11:59:33 -0300

    Implements POST and GET [Spots]

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,19 @@
+const multer = require('multer');
 const express = require('express');
 
+const uploadConfig = require('./config/upload');
 const Spot = require('./controllers/SpotController');
 const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
+// Sessions
 routes.post('/sessions', SessionController.store);
 
-routes.post('/spots', Spot.store);
+// Spots
+routes.get('/spots', Spot.index);
+
+routes.post('/spots', upload.single('thumbnail'), Spot.store);
 
 module.exports = routes;
