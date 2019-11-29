56902119a2b2687d97b62799be59e33cabcd33bf
Leocaliban <leocaliban@gmail.com>
2019-11-29 14:26:47 -0300

    Implementa Dashboard e Booking

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,10 @@ const multer = require('multer');
 const express = require('express');
 
 const uploadConfig = require('./config/upload');
-const Spot = require('./controllers/SpotController');
+const SpotController = require('./controllers/SpotController');
 const SessionController = require('./controllers/SessionController');
+const BookingController = require('./controllers/BookingController');
+const DashboardController = require('./controllers/DashboardController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -12,8 +14,14 @@ const upload = multer(uploadConfig);
 routes.post('/sessions', SessionController.store);
 
 // Spots
-routes.get('/spots', Spot.index);
+routes.get('/spots', SpotController.index);
 
-routes.post('/spots', upload.single('thumbnail'), Spot.store);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+routes.post('/spots/:spot_id/bookings', BookingController.store);
+
+// Dashboard
+routes.get('/dashboard', DashboardController.show);
+
+
 
 module.exports = routes;
