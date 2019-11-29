29ce0f2a08b6eaf4655d1bd24687be4cd33beecf
Leocaliban <leocaliban@gmail.com>
2019-11-29 07:47:58 -0300

    Create Spot resource

---
+++ backend/src/controllers/SpotController.js
@@ -0,0 +1,10 @@
+const Spot = require('../models/Spot');
+
+module.exports = {
+    async store(req, res) {
+
+        return res.json({
+            message: 'SPOT WORKS'
+        });
+    }
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
+
+const Spot = require('./controllers/SpotController');
 const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.post('/spots', Spot.store);
 
 module.exports = routes;
