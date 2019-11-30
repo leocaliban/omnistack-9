4003547181f90ca1524db25066166ac8af38519a
Leocaliban <leocaliban@gmail.com>
2019-11-30 20:13:55 -0300

    Add Route to get images [backend]

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 mongoose.connect('mongodb+srv://leocaliban:leocaliban@cluster0-lt070.mongodb.net/09?retryWrites=true&w=majority', {
@@ -11,6 +12,7 @@ mongoose.connect('mongodb+srv://leocaliban:leocaliban@cluster0-lt070.mongodb.net
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uplpads')));
 app.use(routes);
 
 app.listen(3333);
