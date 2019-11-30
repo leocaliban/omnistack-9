c36c0776150fe616e020de64a0bc793658fad215
Leocaliban <leocaliban@gmail.com>
2019-11-30 19:06:25 -0300

    Implements POST in sessions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import api from './services/api';
 
 import logo from './assets/logo.svg';
 
 function App() {
+
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/sessions', { email });
+
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="Logo" />
@@ -13,12 +27,14 @@ function App() {
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-mail</label>
           <input
             id="email"
             type="email"
             placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
           />
 
           <button className="btn" type="submit">Entrar</button>
