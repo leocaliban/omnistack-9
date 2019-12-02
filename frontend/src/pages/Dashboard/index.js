4ae4f63a5073be41f896e89cd2269f82d2c13e86
Leocaliban <leocaliban@gmail.com>
2019-12-02 19:15:24 -0300

    Create notification block and add styles

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,26 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
 import socketIO from 'socket.io-client';
 
 import './styles.css';
+import { request } from 'http';
 
 export default function Dashboard() {
 
     const [spots, setSpots] = useState([]);
+    const [requests, setRequests] = useState([]);
+
+    const user_id = localStorage.getItem('user');
+    const socket = useMemo(() => socketIO('http://localhost:3333', {
+        query: {
+            user_id
+        }
+    }), [user_id]);
 
     useEffect(() => {
-        const user_id = localStorage.getItem('user');
-        const socket = socketIO('http://localhost:3333', {
-            query: {
-                user_id
-            }
-        });
-
         socket.on('booking_request', data => {
-            console.log(data);
+            setRequests([...requests, data])
         });
-    }, []);
+    }, [requests, socket]);
 
     useEffect(() => {
         async function loadSpots() {
@@ -37,6 +39,21 @@ export default function Dashboard() {
 
     return (
         <>
+            <ul className="notifications">
+                {requests.map(request => (
+                    <li key={request._id}>
+                        <p>
+                            <strong>{request.user.email}</strong>
+                            está solicitando uma reserva em
+                            <strong>{request.spot.company}</strong>
+                            para a data:
+                            <strong>{request.date}</strong>
+                        </p>
+                        <button className="accept">ACEITAR</button>
+                        <button className="reject">REJEITAR</button>
+                    </li>
+                ))}
+            </ul>
             <ul className="spot-list">
                 {spots.map(spot => (
                     <li key={spot._id}>
