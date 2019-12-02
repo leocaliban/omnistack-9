c3960d10796704078c2df0c5026ea031db7175d6
Leocaliban <leocaliban@gmail.com>
2019-12-01 22:42:14 -0300

    Implements Book page

--- mobile/src/pages/Book.js
+++ mobile/src/pages/Book.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import {
     Text,
     View,
     Image,
+    Alert,
     Platform,
     FlatList,
     TextInput,
@@ -14,9 +15,96 @@ import {
     KeyboardAvoidingView,
 } from 'react-native';
 
+import api from '../services/api';
+
 export default function Book({ navigation }) {
+    const [date, setDate] = useState('');
     const id = navigation.getParam('spotId');
+
+    async function handleSubmit() {
+        const user_id = await AsyncStorage.getItem('user');
+
+        await api.post(`/spots/${id}/bookings`, {
+            date
+        }, {
+            headers: { user_id }
+        });
+
+        Alert.alert('Solicitação de reserva enviada.');
+
+        navigation.navigate('List');
+    }
+
+    function handleCancel() {
+        navigation.navigate('List');
+    }
     return (
-        <Text>{id}</Text>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.label}>Data de interesse</Text>
+            <TextInput
+                style={styles.input}
+                placeholder='Qual data você quer reservar?'
+                placeholderTextColor='#999'
+                autoCapitalize='words'
+                autoCorrect={false}
+                value={date}
+                onChangeText={setDate}
+            />
+
+            <TouchableOpacity
+                style={styles.button}
+                onPress={handleSubmit}
+            >
+                <Text style={styles.buttonText}>Solicitar Reserva</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                style={[styles.button, styles.cancelButton]}
+                onPress={handleCancel}
+            >
+                <Text style={styles.buttonText}>Cancelar</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 30,
+        marginTop: 50
+    },
+    label: {
+        marginTop: 10,
+        fontWeight: 'bold',
+        color: '#444',
+        marginBottom: 8,
+        textTransform: 'uppercase'
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 2
+    },
+    button: {
+        height: 42,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+        marginTop: 10
+    },
+    cancelButton: {
+        backgroundColor: '#ccc',
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 16
+    }
+})
